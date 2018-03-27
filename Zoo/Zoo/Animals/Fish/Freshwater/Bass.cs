2e3a85040533703505b3e5755497061d1ee35955
Joshua Taylor <taylor.joshua88@gmail.com>
2018-03-26 21:31:08 -0700

    All tests passing

--- Zoo/Zoo/Animals/Fish/Freshwater/Bass.cs
+++ Zoo/Zoo/Animals/Fish/Freshwater/Bass.cs
@@ -7,7 +7,7 @@ namespace Zoo.Animals.Fish.Freshwater
     /// <summary>
     /// Class for the bass in our zoo
     /// </summary>
-    class Bass : Freshwater
+    public class Bass : Freshwater
     {
         public override string Name { get; protected set; } = "Bass";
         protected override string Diet { get; set; } = "Fish";
